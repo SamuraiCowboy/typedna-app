d8ff8c7f14d7c720565fd17960aba8cdf79cf0ee
Faeez Shaikh <faeez.shaikh@gmail.com>
2020-05-30 14:15:13 -0500

    Data

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -21,28 +21,61 @@ export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
 
   columnDefs = [
-    {headerName: 'No', field: 'index', sortable: true, resizable:true, filter: true,checkboxSelection:true,rowDrag: true,headerCheckboxSelection: true,headerCheckboxSelectionFilteredOnly: true},
-    {headerName: 'Job Id', field: 'id', sortable: true, resizable:true,filter: true,   enableCellChangeFlash: true},
-    {headerName: 'Job Name', field: 'email', sortable: true, resizable:true,filter: true,   enableCellChangeFlash: true},
-    {headerName: 'Verifier', field: 'verifier', sortable: true, resizable:true,filter: true},
+    {headerName: 'Jenkins Job Id', field: 'jenkinsJobId', sortable: true, resizable:true, filter: true,checkboxSelection:true,rowDrag: true,headerCheckboxSelection: true,headerCheckboxSelectionFilteredOnly: true},
+    {headerName: 'User Product Guid', field: 'userProductGuid', sortable: true, resizable:true,filter: true,   enableCellChangeFlash: true},
+    {headerName: 'File Name', field: 'fileName', sortable: true, resizable:true,filter: true,   enableCellChangeFlash: true},
     {headerName: 'Start Time', field: 'startTime', sortable: true,resizable:true, filter: true,   enableCellChangeFlash: true},
-    {headerName: 'Is Active', field: 'isActive', sortable: true,resizable:true, filter: true,   enableCellChangeFlash: true},
-   {headerName: 'Status', field: 'color', sortable: true,resizable:true, filter: true,   cellStyle: function(params) {
-      if (params.value=='brown') {
+    {headerName: 'End Time', field: 'endTime', sortable: true,resizable:true, filter: true,   enableCellChangeFlash: true},
+    {headerName: 'Org', field: 'org', sortable: true, resizable:true,filter: true},
+    {headerName: 'Location', field: 'loc', sortable: true, resizable:true,filter: true},
+    {headerName: 'Total SSNs', field: 'totalSsns', sortable: true,resizable:true, filter: true,   enableCellChangeFlash: true},
+   {headerName: 'Stoppable', field: 'stoppable', sortable: true,resizable:true, filter: true,   enableCellChangeFlash: true},
+   {headerName: 'Status', field: 'status', sortable: true,resizable:true, filter: true,   cellStyle: function(params) {
+      if (params.value=='Stopped') {
           //mark police cells as red
           return {color: 'black', backgroundColor: 'rgba(255,128,171,.4)'};
-      } if (params.value=='blue') {
+      } if (params.value=='Completed') {
         //mark police cells as red
         return {color: 'black', backgroundColor: 'cyan'};
-    } if (params.value=='green') {
+    } if (params.value=='Aborted') {
       //mark police cells as red
       return {color: 'black', backgroundColor: 'rgba(185,246,202,.4)'};
-  } else {
+  } if (params.value=='Cancelled') {
+    //mark police cells as red
+    return {color: 'black', backgroundColor: 'orange'};
+}if (params.value=='Running') {
+  //mark police cells as red
+  return {color: 'black', backgroundColor: 'green'};
+}else {
           return null;
       }
   }},
-  {headerName: 'Phone', field: 'phone', sortable: true, resizable:true,filter: true},
-  {headerName: 'Comments', field: 'comments', sortable: true,resizable:true, filter: true}
+  {headerName: 'Paychex Opted In', field: 'payxOptedIn', sortable: true,resizable:true, filter: true,   enableCellChangeFlash: true},
+  {headerName: 'NR Opted In', field: 'nrOptedIn', sortable: true,resizable:true, filter: true,   enableCellChangeFlash: true},
+  {headerName: 'Paychex SSNs', field: 'payxSsns', sortable: true,resizable:true, filter: true,   enableCellChangeFlash: true},
+  
+
+  {headerName: 'Sub Status', field: 'subStatus', sortable: true,resizable:true, filter: true,   cellStyle: function(params) {
+    if (params.value=='Stopped') {
+        //mark police cells as red
+        return {color: 'black', backgroundColor: 'rgba(255,128,171,.4)'};
+    } if (params.value=='Completed') {
+      //mark police cells as red
+      return {color: 'black', backgroundColor: 'cyan'};
+  } if (params.value=='Aborted') {
+    //mark police cells as red
+    return {color: 'black', backgroundColor: 'rgba(185,246,202,.4)'};
+} if (params.value=='Cancelled') {
+  //mark police cells as red
+  return {color: 'black', backgroundColor: 'orange'};
+}if (params.value=='Running') {
+//mark police cells as red
+return {color: 'black', backgroundColor: 'green'};
+}else {
+        return null;
+    }
+}}
+
 ];
 rowData:any;
 rowDataClicked1 = {};
@@ -56,7 +89,7 @@ gridOptions;
   ) {}
 
   ngOnInit() {
-    this.rowData = this.firestore.collection('items').valueChanges();
+    this.rowData = this.firestore.collection('jobs').valueChanges();
   //  this.rowData = this.placesService.getPlaces();
    this.themevar = "ag-theme-balham-dark";
 
@@ -64,7 +97,7 @@ gridOptions;
 
   refresh(){
     // this.rowData = this.placesService.getPlaces();
-    this.rowData = this.firestore.collection('items').valueChanges();
+    this.rowData = this.firestore.collection('jobs').valueChanges();
 
   }
   changeTheme(){
